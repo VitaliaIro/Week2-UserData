74ea55575b4320329f62d0d22b8fe19a521a9fc5
Õpilane 53KK <opilane53@edu.lan>
2022-02-11 20:18:54 +0200

    Control Flow

---
+++ UserAge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaastat;
+            //programm kuvab sõnniaastat konsoolis;
+            //"Oled sündinud aastal {yearOfBirth}"
+
+            Console.WriteLine("Palun, sisesta oma vanus:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            int yearOfBirth = 2021 - age;
+
+            Console.WriteLine($"Oled sündinud aastal {yearOfBirth}");
+
+        }
+    }
+}
